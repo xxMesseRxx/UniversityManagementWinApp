d11203851f48c15a25b264f3609fbe606ee553d2
xxMesseRxx <343454640@mail.ru>
2023-03-29 22:12:38 +0500

    In model was added interface implementation: "INotifyPropertyChanged"

--- UniversityWPF/Model/Group.cs
+++ UniversityWPF/Model/Group.cs
@@ -1,13 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace UniversityWPF.Model;
 
-public class Group
+public class Group : INotifyPropertyChanged
 {
-    public int GroupId { get; set; }
-    public int CourseId { get; set; }
-    public string Name { get; set; } = null!;
-    public Course Course { get; set; } = null!;
-    public List<Student> Students { get; set; } = new List<Student>();
+    public int GroupId
+	{
+		get { return _groupId; }
+		set
+		{
+			_groupId = value;
+			OnPropertyChanged();
+		}
+	}
+    public int CourseId
+	{
+		get { return _courseId; }
+		set
+		{
+			_courseId = value;
+			OnPropertyChanged();
+		}
+	}
+    public string Name
+	{
+		get { return _name; }
+		set
+		{
+			_name = value;
+			OnPropertyChanged();
+		}
+	}
+    public Course Course
+	{
+		get { return _course; }
+		set
+		{
+			_course = value;
+			OnPropertyChanged();
+		}
+	}
+    public List<Student> Students
+	{
+		get { return _students; }
+		set
+		{
+			_students = value;
+			OnPropertyChanged();
+		}
+	}
+
+	private int _groupId;
+	private int _courseId;
+	private string _name = null!;
+	private Course _course = null!;
+	private List<Student> _students = new List<Student>();
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	public void OnPropertyChanged([CallerMemberName] string prop = "")
+	{
+		if (PropertyChanged != null)
+			PropertyChanged(this, new PropertyChangedEventArgs(prop));
+	}
 }
--- UniversityWPF/Model/Student.cs
+++ UniversityWPF/Model/Student.cs
@@ -1,13 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace UniversityWPF.Model;
 
-public class Student
+public class Student : INotifyPropertyChanged
 {
-    public int StudentId { get; set; }
-    public int GroupId { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public Group Group { get; set; } = null!;
+    public int StudentId
+	{
+		get { return _studentId; }
+		set
+		{
+			_studentId = value;
+			OnPropertyChanged();
+		}
+	}
+    public int GroupId
+	{
+		get { return _groupId; }
+		set
+		{
+			_groupId = value;
+			OnPropertyChanged();
+		}
+	}
+    public string FirstName
+	{
+		get { return _firstName; }
+		set
+		{
+			_firstName = value;
+			OnPropertyChanged();
+		}
+	}
+	public string LastName
+	{
+		get { return _lastName; }
+		set
+		{
+			_lastName = value;
+			OnPropertyChanged();
+		}
+	}
+	public Group Group
+	{
+		get { return _group; }
+		set
+		{
+			_group = value;
+			OnPropertyChanged();
+		}
+	}
+
+	private int _studentId;
+	private int _groupId;
+	private string _firstName = null!;
+	private string _lastName = null!;
+	private Group _group = null!;
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	public void OnPropertyChanged([CallerMemberName] string prop = "")
+	{
+		if (PropertyChanged != null)
+			PropertyChanged(this, new PropertyChangedEventArgs(prop));
+	}
 }
