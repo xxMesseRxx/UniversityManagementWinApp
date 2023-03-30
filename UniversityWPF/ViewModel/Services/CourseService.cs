a89cf6dde1cf80de04c9a083f35fd4a50ac8c46e
xxMesseRxx <343454640@mail.ru>
2023-03-30 22:58:31 +0500

    In ICourseService and CourseService method SaveChanges now take Object arg.

---
+++ UniversityWPF/ViewModel/Services/CourseService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniversityWPF.Model;
+using System.Linq;
+using System.Collections.ObjectModel;
+using UniversityWPF.Library.Interfaces;
+using System.Collections.Specialized;
+using System.Windows;
+
+namespace UniversityWPF.ViewModel.Services
+{
+    public class CourseService : ICourseService
+    {
+        private UniversityContext _db;
+
+        public CourseService(UniversityContext context)
+        {
+            _db = context;
+        }
+
+        public ObservableCollection<Course> GetAll()
+        {
+            _db.Courses.Load();
+            return _db.Courses.Local.ToObservableCollection();
+        }
+        public void SaveChanges(object? obj = null)
+        {
+            if (obj is null)
+            {
+                _db.SaveChanges();
+                _db.Courses.ToList().Last().OnPropertyChanged("CourseId");
+            }
+            else if (obj is NotifyCollectionChangedEventArgs arg && arg.Action == NotifyCollectionChangedAction.Remove)
+            {
+                _db.SaveChanges();
+            }
+        }
+    }
+}
