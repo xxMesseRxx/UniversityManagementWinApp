4d337fc28ff0a93fdc1e07bdb45fd07d0e7cc05f
xxMesseRxx <343454640@mail.ru>
2023-03-30 20:33:46 +0500

    In App class was added host for services.

--- UniversityWPF/App.xaml.cs
+++ UniversityWPF/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,34 @@ namespace UniversityWPF
 	/// </summary>
 	public partial class App : Application
 	{
+		private readonly IHost _host;
+
+		public App()
+		{
+			_host = Host.CreateDefaultBuilder()
+			.ConfigureServices(services =>
+			{
+				services.AddSingleton<MainWindow>();
+			})
+			.Build();
+		}
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			_host.Start();
+
+			MainWindow = _host.Services.GetRequiredService<MainWindow>();
+			MainWindow.Show();
+
+			base.OnStartup(e);
+		}
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			_host.StopAsync();
+			_host.Dispose();
+
+			base.OnExit(e);
+		}
 	}
 }
