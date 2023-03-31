c8d37cc857bba4f15593b103c77091ab1fc8b7fc
xxMesseRxx <343454640@mail.ru>
2023-03-31 20:45:15 +0500

    Were removed 2 buttons from Courses tab, set binding between checkbox and isReadOnly field of datagrid and was created new class BoolToOppositeBoolConverter.

---
+++ UniversityWPF/Library/Converters/BoolToOppositeBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UniversityWPF.Library.Converters
+{
+	public class BoolToOppositeBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool))
+			{
+				throw new ArgumentException("Value isn't bool");
+			}
+
+			return !(bool)value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool))
+			{
+				throw new ArgumentException("Value isn't bool");
+			}
+
+			return !(bool)value;
+		}
+	}
+}
