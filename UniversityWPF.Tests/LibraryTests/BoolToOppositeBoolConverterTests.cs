95d5542b08845a5cdb63b48a64db9b0640ff1b5d
xxMesseRxx <343454640@mail.ru>
2023-04-04 21:26:37 +0500

    Was created tests project.

---
+++ UniversityWPF.Tests/LibraryTests/BoolToOppositeBoolConverterTests.cs
@@ -0,0 +1,11 @@
+namespace UniversityWPF.Tests.LibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
