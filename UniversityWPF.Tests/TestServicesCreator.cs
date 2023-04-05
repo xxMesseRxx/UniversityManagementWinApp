64eaf0acde237fa753e8e233cc72747d47c54bd2
xxMesseRxx <343454640@mail.ru>
2023-04-05 23:07:45 +0500

    In tests project were added "TestDBCreator" and "TestServicesCreator" classes. Were added few tests for CourseService class.

---
+++ UniversityWPF.Tests/TestServicesCreator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityWPF.Model;
+using UniversityWPF.ViewModel.Services;
+
+namespace UniversityWPF.Tests
+{
+	public static class TestServicesCreator
+	{
+		private static IServiceCollection _services;
+
+		static TestServicesCreator()
+		{
+			_services = new ServiceCollection()
+							.AddDbContext<UniversityContext>(options =>
+															 options.UseSqlServer("Server=localhost;Database=TestUniversity;Trusted_Connection=True;Encrypt=False;"),
+															 ServiceLifetime.Scoped);
+		}
+
+		public static CourseService GetCourseService()
+		{
+			return new CourseService(_services.BuildServiceProvider());
+		}
+		public static GroupService GetGroupService()
+		{
+			return new GroupService(_services.BuildServiceProvider());
+		}
+		public static StudentService GetStudentService()
+		{
+			return new StudentService(_services.BuildServiceProvider());
+		}
+	}
+}
---
+++ UniversityWPF.Tests/ViewModelTests/CourseServiceTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityWPF.Library;
+using UniversityWPF.Model;
+using UniversityWPF.ViewModel.Services;
+
+namespace UniversityWPF.Tests.ViewModelTests
+{
+	[TestClass]
+	public class CourseServiceTests
+	{
+		[TestMethod]
+		public void CoursesCollecton_Get_ListWith9CoursesExpected()
+		{
+			var dbCreator = new TestDBCreator();
+			try
+			{
+				//Arrange
+				dbCreator.CreateTestDB();
+				CourseService courseService = TestServicesCreator.GetCourseService();
+				int expected = 9;
+
+				//Act
+				ObservableCollection<Course> courses = courseService.Courses;
+				int actual = courses.Count();
+
+				//Assert
+				Assert.AreEqual(expected, actual);
+			}
+			finally
+			{
+				dbCreator.Dispose();
+			}
+		}
+
+		[TestMethod]
+		public void SaveChangesCommand_ChangeNameToCorName_CourseNameWasChangedExpected()
+		{
+			var dbCreator = new TestDBCreator();
+			try
+			{
+				//Arrange
+				dbCreator.CreateTestDB();
+				CourseService courseService = TestServicesCreator.GetCourseService();
+				RelayCommand saveCmd = courseService.SaveChangesCommand;
+				ObservableCollection<Course> courses = courseService.Courses;
+				string expected = "New name";
+
+				//Act
+				courses[0].Name = expected;
+				saveCmd.Execute(courses[0]);
+
+				string actual = courses[0].Name;
+
+				//Assert
+				Assert.AreEqual(expected, actual);
+			}
+			finally
+			{
+				dbCreator.Dispose();
+			}
+		}
+		[TestMethod]
+		public void SaveChangesCommand_ChangeNameToEmpty_CourseNameWasNotChangedExpected()
+		{
+			var dbCreator = new TestDBCreator();
+			try
+			{
+				//Arrange
+				dbCreator.CreateTestDB();
+				CourseService courseService = TestServicesCreator.GetCourseService();
+				RelayCommand saveCmd = courseService.SaveChangesCommand;
+				ObservableCollection<Course> courses = courseService.Courses;
+				string expected = courses[0].Name;
+
+				//Act
+				courses[0].Name = "";
+				saveCmd.Execute(courses[0]);
+
+				string actual = courses[0].Name;
+
+				//Assert
+				Assert.AreEqual(expected, actual);
+			}
+			finally
+			{
+				dbCreator.Dispose();
+			}
+		}
+	}
+}
